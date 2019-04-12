4636f97b796a6db6ff15c30f7a530ac471b9baf5
StefanC <cotargasanu.stef@gmail.com>
2019-04-11 22:27:04 -0400

    Modified admins list

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-password'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-not-a-password'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -10,5 +10,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['email@example.com']
-    GRADES_PER_PAGE = 5
+    ADMINS = ['cotargasanu.stef@gmail.com']
+    GRADES_PER_PAGE = 8
